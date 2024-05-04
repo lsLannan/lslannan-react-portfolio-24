224702d4d0216c8ee2661c9fd85c13c58573475d
Liberty <49573531+lsLannan@users.noreply.github.com>
2024-05-04 20:02:00 +0100

    optimised smaller mobile screens responsivity

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,7 @@ import '../styles/Experience.css';
 
 export default function Experience() {
     return (
-        <div>
+        <div className='experience-container'>
             <h1>EXPERIENCE</h1>
             <img className='timeline' src={require('../images/experience-timeline-v2.png')} alt="experience timeline"/>
             <img className='mobile-timeline' src={require('../images/mobile-timeline.png')} alt="experience timeline"/>
